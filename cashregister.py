51523de972d590a7d72ef7acb13f286950bdeba5
John Maheswaran <john.maheswaran@yale.fake>
2020-02-09 00:34:40 -0500

    prefix matching in sub O(n) time using a trie

--- cashregister.py
+++ cashregister.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import csv # used to read csv files 
+from pytrie import StringTrie # used to match prefixes in O(log(l)) time where 
+# l is the length of the string, better than O(n) time which would be the case
+# if we were naively to look through all strings, if there are n strings
 
+products = dict() # dict representing the products, allows O(1) lookup of price 
+# informaton based on the product id
 
-import csv
-
-products = dict() # dict representing the products
+trie=StringTrie() # a trie that allows us to look up all the strings matching a
+# prefix in O(log(l)) time where l 
 
 # when to program starts it reads the product data from the productdata.csv file
 with open('productdata.csv', newline='') as csvfile:
@@ -14,9 +19,27 @@ with open('productdata.csv', newline='') as csvfile:
 		name = row[1]
 		price = row[2]
 		category = row[3]
+		# add the product to the product map
 		products[id] = {"name" : name, "price" : price, "category" : category}
+		# add the product to the trie
+		trie[id] = (id, name)
+
+
+while True:
+	print("Please enter a prefix (ctrl-c to exit): ")
+	prefix = input()
+	got = trie.values(prefix)
+	if len(got) == 0:
+		print("No matches")
+	elif len(got) == 1:
+		print("Ringing up: ")
+		print(got)
+	else:
+		print("Matches: ")
+		print(got)
+
 		
-print(products)
+
 		
 
 
