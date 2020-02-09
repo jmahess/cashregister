5b858637b7c6951c58a41842aa07ad0f077d5d08
John Maheswaran <john.maheswaran@yale.fake>
2020-02-09 01:04:39 -0500

    ringing up items

--- cashregister.py
+++ cashregister.py
@@ -8,6 +8,8 @@ from pytrie import StringTrie # used to match prefixes in O(log(l)) time where
 products = dict() # dict representing the products, allows O(1) lookup of price 
 # informaton based on the product id
 
+rungup = set() # this stores the products that we have rung up
+
 trie=StringTrie() # a trie that allows us to look up all the strings matching a
 # prefix in O(log(l)) time where l 
 
@@ -22,18 +24,38 @@ with open('productdata.csv', newline='') as csvfile:
 		# add the product to the product map
 		products[id] = {"name" : name, "price" : price, "category" : category}
 		# add the product to the trie
-		trie[id] = (id, name)
+		trie[id] = (id, name, price)
 
 
 while True:
 	print()
-	print("Please enter a product identifier or type 'q' to quit: ")
+	print("Please enter a product identifier or type 'h' for help: ")
 	print()
 
 	prefix = input()
 	print()
+
+	if prefix == "h" or prefix == "help":
+		print("Use the following commands:")
+		print("%-15s %40s" %("Command", "Description"))
+		print("%-15s %40s" %("help", "List help"))
+		print("%-15s %40s" %("quit", "Quit / exit the program"))
+		print("%-15s %40s" %("rungup", "List the rung up items"))
+		print("%-15s %40s" %("total", "Calculate the total bill and finish"))
+		continue
+
+
 	if prefix == "q" or prefix == "quit" or prefix == "exit":
 		break
+
+	if prefix == "r" or prefix == "rungup":
+		print("Rung up items so far: ")
+		print()
+		print("%-18s %-9s" %("Product name", "Price"))	
+		for r in rungup:	
+			print("%-18s %-9s" %(products[r]["name"], products[r]["price"]))		
+		continue
+
 	got = trie.values(prefix)
 
 	if len(got) == 0:
@@ -41,8 +63,13 @@ while True:
 	elif len(got) == 1:
 		print("Ringing up: ")
 		print()
-		print("%-15s %-20s" %("Product ID", "Product name"))		
-		print("%-15s %-20s" %(g[0], g[1]))
+		print("%-18s %-9s" %("Product name", "Price"))		
+		row = got[0]
+		id = row[0]
+		name = row[1]
+		price = row[2]
+		print("%-18s %-9s" %(name, price))
+		rungup.add(id) # add the product ID to the set of rung up items
 	else:
 		print("Mutiple matching products found: ")
 		print()		
