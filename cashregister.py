e07cff4ecb3bbe94ecd78feeab4f10f1329794bc
John Maheswaran <john.maheswaran@yale.fake>
2020-02-09 21:18:04 -0500

    cleaning up the formatting of dollar amounts

--- cashregister.py
+++ cashregister.py
@@ -4,6 +4,7 @@ import csv # used to read csv files
 from pytrie import StringTrie # used to match prefixes in better than O(n) time.
 # O(n) time would be the case if we did not use a trie and instead naively looked
 #  through all the n strings and check one by one
+import locale # used for formatting the currency in prices
 
 products = dict() # a dict is a hashmap representing the products, allows O(1) lookup of product price 
 # informaton based on the product id
@@ -12,6 +13,9 @@ rungup = set() # this stores the products that we have rung up and allows O(1) i
 
 trie=StringTrie() # a trie that allows us to look up all the strings matching a
 # prefix in faster than O(n) time where n is the number of strings
+
+# set the locate for currency formatting
+locale.setlocale( locale.LC_ALL, '' )
 
 # when to program starts it reads the product data from the productdata.csv file
 with open('productdata.csv', newline='') as csvfile:
@@ -54,7 +58,13 @@ while True:
 
 	# functionality to total up and calculate taxes
 	if userInput == "t" or userInput == "total":
-		print("%-18s %-9s" %("Product name", "Price"))	
+		# do not compute the total if no items have been added, instead tell
+		# the user to add some items
+		if len(rungup) == 0:
+			print("Please ring up some items before calculating the total")
+			continue
+
+		print("%-18s %9s" %("Product name", "Price"))	
 		total = 0 # the total amount
 		totalCountyTaxable = 0 # the amount taxable by the county
 		for r in rungup:	
@@ -64,38 +74,47 @@ while True:
 				totalCountyTaxable += float(products[r]["price"]) # only add non grocery items to 
 				# the county taxable amount
 		print()
-		print("%-18s %-9s" %("Subtotal: ", total))
+		print("%-18s %9s" %("Subtotal: ", locale.currency(total)))
 		# add up the tax, 6.3% state + 2% city (8.3%) 
 		# on everything, and 0.7% county on non grocery items. Round to 2 decimal places, dollars and cents		
 		statetax = round(total*0.063, 2)
 		countytax = round(totalCountyTaxable*0.007, 2)
 		citytax = round(total*0.02, 2)
 		tax = round(statetax + countytax + citytax, 2)
-		print("%-18s %-9s" %("Tax: ", tax))
-		print("%-18s %-9s" %("Total due: ", round(tax + total, 2)))
+		print("%-18s %9s" %("Tax: ", locale.currency(tax)))
+		print("%-18s %9s" %("Total due: ", locale.currency(round(tax + total, 2))))
 		print()
 
 		# get the amount paid by the customer
-		print("Please enter the amount paid by the customer:")
-		customerAmount = float(input())
+		customerAmount = 0
+		while customerAmount < round(tax + total, 2):
+			print("Please enter the amount paid by the customer:")
+			inputFromUser = input()
+			try:
+				customerAmount = float(inputFromUser)	
+			except:
+				print("Please enter a numerical value")
+			if customerAmount < round(tax + total, 2):
+				print("The amount paid by the customer must not be less than the amount due")
+
 		print()
 		print("Receipt:")
 		print()
-		print("%-18s %-18s %-10s %-17s" %("Product name", "Product identifier", "Price", "Tax category"))	
+		print("%-18s %-18s %10s %-17s" %("Product name", "Product identifier", "Price", "Tax category"))	
 
 		for id in rungup:	
-			print("%-18s %-18s %-10s %-17s" %(products[id]["name"], id, products[id]["price"], products[id]["category"]))	
+			print("%-18s %-18s %10s %-17s" %(products[id]["name"], id, locale.currency(float(products[id]["price"])), products[id]["category"]))	
 		print()
 		# print out the subtotal and taxes
-		print("%-18s %-9s" %("Subtotal: ", total))
-		print("%-18s %-9s" %("State tax: ", statetax))
-		print("%-18s %-9s" %("County tax: ", countytax))	
-		print("%-18s %-9s" %("City tax: ", citytax))	
+		print("%-18s %9s" %("Subtotal: ", locale.currency(total)))
+		print("%-18s %9s" %("State tax: ", locale.currency(statetax)))
+		print("%-18s %9s" %("County tax: ", locale.currency(countytax)))	
+		print("%-18s %9s" %("City tax: ", locale.currency(citytax)))	
 		# print the total amount due, the amount paid by the customer, and the amount of change
-		print("%-18s %-9s" %("Total due: ", round(tax + total, 2)))
-		print("%-18s %-9s" %("Amount paid: ", customerAmount))
+		print("%-18s %9s" %("Total due: ", locale.currency(round(tax + total, 2))))
+		print("%-18s %9s" %("Amount paid: ", locale.currency(customerAmount)))
 		change = customerAmount - tax - total
-		print("%-18s %-9s" %("Change: ", round(change, 2)))
+		print("%-18s %9s" %("Change: ", locale.currency(round(change, 2))))
 		print()
 		print("Thank you! Have a nice day!")
 		break
@@ -109,10 +128,10 @@ while True:
 	if userInput == "r" or userInput == "rungup":
 		print("Rung up items so far: ")
 		print()
-		print("%-18s %-9s" %("Product name", "Price"))	
+		print("%-18s %9s" %("Product name", "Price"))	
 		# iterate through all the rung up items and print them out
 		for r in rungup:	
-			print("%-18s %-9s" %(products[r]["name"], products[r]["price"]))		
+			print("%-18s %9s" %(products[r]["name"], locale.currency(float(products[r]["price"]))))		
 		continue
 
 	# matching values
@@ -125,13 +144,13 @@ while True:
 	elif len(matches) == 1:
 		print("Ringing up: ")
 		print()
-		print("%-18s %-9s" %("Product name", "Price"))		
+		print("%-18s %9s" %("Product name", "Price"))		
 		# get the item and the relevant fields
 		item = matches[0]
 		id = item[0]
 		name = item[1]
 		price = item[2]
-		print("%-18s %-9s" %(name, price)) # print the name and price our
+		print("%-18s %9s" %(name, locale.currency(float(price)))) # print the name and price our
 		rungup.add(id) # add the product ID to the set of rung up items
 	# case where multiple products match
 	else:
